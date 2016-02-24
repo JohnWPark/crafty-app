83a6ba5806871125f4a1504a6bc654164c5b3edd
John Park <jwpark417@gmail.com>
2016-02-24 16:41:58 -0500

    Adding camera functionality with Cordova

--- www/js/controllers/photoDetail.js
+++ www/js/controllers/photoDetail.js
@@ -1,15 +1,11 @@
 'use strict';
 
-app.controller('PhotoDetailCtrl', function($scope, Chats, Products) {
+app.controller('PhotoDetailCtrl', function($scope, Products, $rootScope) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-    $scope.image = 'http://www.smashbros.com/us/images/character/cloud/illust-modal.jpg';
+  $scope.imgURI = $rootScope.imgURI;
 
     $scope.button = function(product){
       console.log('button was clicked', product, $scope.image);
-      Products.saveProduct(product, $scope.image);
+      Products.saveProduct(product, $scope.imgURI);
   }
 })
