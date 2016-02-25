d28ebd84834063187e71e0b341cc7b295967f6e6
John Park <jwpark417@gmail.com>
2016-02-24 21:45:29 -0500

    Refactor code for Cordova

--- www/js/controllers/photo.js
+++ www/js/controllers/photo.js
@@ -2,21 +2,21 @@
 
 app.controller('PhotoCtrl', function($scope, $cordovaCamera, $rootScope, $state) {
 
+  var options = {
+    quality: 75,
+    destinationType: Camera.DestinationType.DATA_URL,
+    allowEdit: true,
+    encodingType: Camera.EncodingType.JPEG,
+    popoverOptions: CameraPopoverOptions,
+    targetWidth: 500,
+    targetHeight: 500,
+    saveToPhotoAlbum: false
+  };
+
   $scope.takePhoto = function(){
     console.log('takePhoto was clicked');
     //Camera Plugin
-
-    var options = {
-      quality: 75,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      popoverOptions: CameraPopoverOptions,
-      targetWidth: 500,
-      targetHeight: 500,
-      saveToPhotoAlbum: false
-    };
+    options.sourceType = Camera.PictureSourceType.CAMERA;
 
     $cordovaCamera.getPicture(options).then(function(photo){
       $rootScope.imgURI = "data:image/jpeg;base64," +photo;
@@ -26,5 +26,11 @@ app.controller('PhotoCtrl', function($scope, $cordovaCamera, $rootScope, $state)
 
   $scope.choosePhoto = function(){
     console.log('will link up to photoLibrary');
+    options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+
+    $cordovaCamera.getPicture(options).then(function(photo){
+      $rootScope.imgURI = "data:image/jpeg;base64," +photo;
+      $state.go('tab.photo-detail');
+    })
   }
 });
